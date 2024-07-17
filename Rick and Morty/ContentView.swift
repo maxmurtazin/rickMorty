4f47f57b7d9e30e616411e2dc8461a7b85d0db17
max <murtazin@chem.vsu.ru>
2024-07-17 15:30:44 +0300

    Initial Commit

---
+++ Rick and Morty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick and Morty
+//
+//  Created by Patric Bell on 17.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
